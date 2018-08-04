dbea9035f2d9a9ad796be22d04191a07cb351c4c
Paolo <mrtnpaolo@gmail.com>
2018-08-05 01:17:55 +0200

    🌲 day-04 part 2

--- day-04/Main.hs
+++ day-04/Main.hs
@@ -3,13 +3,17 @@
 module Main where
 
 import Data.Ord      ( comparing )
-import Data.List     ( group, groupBy, sort, sortBy )
+import Data.List     ( group, groupBy, sort, sortBy, intercalate, tails )
+import Data.Char     ( ord, chr )
 import Data.Function ( on )
 
 data Room = Room { _name :: [String], _sector :: Int, _checksum :: String }
   deriving Show
 
-main = print . sum . map _sector . filter realRoom =<< readInput
+main =
+  do input <- readInput
+     print . sum . map _sector . filter realRoom $ input
+     mapM_ print . filter (roomNameContains "north") . map decrypt $ input
 
 readInput = map (fromList . words . map replace) . lines <$> readFile "input.txt"
   where
@@ -19,3 +23,12 @@ readInput = map (fromList . words . map replace) . lines <$> readFile "input.txt
         (c:s:n) = reverse xs
 
 realRoom Room{..} = _checksum == (take 5 . map head . concat . map sort . groupBy ((==) `on` length) . reverse . sortBy (comparing length) . group . sort . concat $ _name)
+
+decrypt r@Room{..} = r { _name = [decrypted] }
+  where
+    decrypted = intercalate " " $ map (map rotate) _name
+    rotate = chr . (+ ord 'a') . (`mod` (ord 'z' - ord 'a' + 1)) . (+ _sector) . subtract (ord 'a') . ord
+
+roomNameContains name Room{..} = any (== name) substrings
+  where
+    substrings = map (take (length name)) . tails . concat $ _name
