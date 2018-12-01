ab45b053f13bfb4adfac9004fbc86b59a8e55cfe
Paolo <mrtnpaolo@gmail.com>
2018-12-01 23:15:47 +0100

    🎄 day-07 in C part 2

--- day-07/main.c
+++ day-07/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+#define MAXABA 1024
+
+struct ssl
+{
+	struct
+	{
+		char a;
+		char b;
+	} ab[MAXABA];
+	size_t count;
+};
+
+void
+dump (struct ssl * s)
+{
+	for (size_t i = 0; i < s->count; ++i)
+		printf("\t%c%c%c", s->ab[i].a, s->ab[i].b, s->ab[i].a);
+	printf("\n");
+}
 
 #define MAXIPS 2048
 #define MAXIPv7LEN 1024
@@ -10,6 +31,7 @@ struct state
 	char ips[MAXIPS][MAXIPv7LEN];
 	size_t ip_count;
 	size_t tls_count;
+	size_t ssl_count;
 };
 
 size_t
@@ -27,12 +49,23 @@ isTLS_ (char *, bool);
 bool
 hasABBA (char *);
 
+size_t
+countSSL (struct state *);
+
+bool
+isSSL (char *);
+
+size_t
+collectABA (struct ssl *, char *);
+
 int
 main (void)
 {
-	struct state s = { 0 };
+	struct state s = { 0 }, s1;
 	printf("total:\t%ld\n", parse(&s));
+	memcpy((void *)&s1, (void *)&s, sizeof(struct state));
 	printf("tls:\t%ld\n", countTLS(&s));
+	printf("ssl:\t%ld\n", countSSL(&s1));
 }
 
 size_t
@@ -93,5 +126,77 @@ hasABBA (char * str)
 		}
 	}
 	return false;
+}
+
+size_t
+countSSL (struct state * s)
+{
+	for (size_t i = 0; i < s->ip_count; ++i)
+	{
+		if (isSSL(s->ips[i]))
+			++s->ssl_count;
+	}
+	return s->ssl_count;
+}
+
+bool
+isSSL (char * str)
+{
+	struct ssl s_out = { 0 };
+	struct ssl s_in  = { 0 };
+
+	char * start = str;
+	while (*start)
+	{
+		char * end = start;
+		while (*end != '\0' && *end != '[' && *end != ']')
+			++end;
+		if (*end == ']')
+		{
+			*end = '\0';
+			collectABA(&s_in, start);
+		}
+		else
+		{
+			*end = '\0';
+			collectABA(&s_out, start);
+		}
+		start = ++end;
+	}
+	//printf("%.20s...\t%zu\t%zu\n", str, s_out.count, s_in.count);
+	//dump(&s_out);
+	//dump(&s_in);
+
+	if (s_out.count && s_in.count)
+		for (size_t i = 0; i < s_out.count; ++i)
+			for (size_t j = 0; j < s_in.count; ++j)
+				if (  (s_out.ab[i].a == s_in.ab[j].b)
+				   && (s_out.ab[i].b == s_in.ab[j].a) )
+					return true;
+
+	return false;
+}
+
+size_t
+collectABA (struct ssl * s, char * str)
+{
+	char * p = str;
+
+	for (size_t i = 0; *p != '\0'; ++i, ++p)
+	{
+		if (i > 1)
+		{
+			if ( *(p) == *(p-2) && *(p) != *(p-1) )
+			{
+				s->ab[s->count].a = *p;
+				s->ab[s->count].b = *(p-1);
+				++s->count;
+			}
+		}
+	}
+
+//	printf("%.10s...\t%zu\n", str, s->count);
+
+	return s->count;
 }
 
